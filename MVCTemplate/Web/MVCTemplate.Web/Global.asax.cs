e31165821db2d167292d87da79b2e052d39cc2cd
ahmed <ahmedmatem@gmail.com>
2016-05-02 12:21:05 +0100

    installing Autofac

--- MVCTemplate/Web/MVCTemplate.Web/Global.asax.cs
+++ MVCTemplate/Web/MVCTemplate.Web/Global.asax.cs
@@ -12,6 +12,7 @@ namespace MVCTemplate.Web
     {
         protected void Application_Start()
         {
+            AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
