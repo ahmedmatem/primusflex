1ba6d85832c1a85d3fb715dea09b19870a8eba87
ahmed <ahmedmatem@gmail.com>
2016-05-02 14:08:23 +0100

    Enable migration

--- MVCTemplate/Web/MVCTemplate.Web/Global.asax.cs
+++ MVCTemplate/Web/MVCTemplate.Web/Global.asax.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace MVCTemplate.Web
+﻿namespace MVCTemplate.Web
 {
+    using System.Data.Entity;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    using Data;
+    using Data.Migrations;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>();
             AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
