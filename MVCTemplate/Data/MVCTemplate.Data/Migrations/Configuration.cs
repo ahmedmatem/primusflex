4f5c1f2f4ad9192f67c396a89307857c2bc14452
ahmed <ahmedmatem@gmail.com>
2016-05-02 14:08:48 +0100

    Enable migration

---
+++ MVCTemplate/Data/MVCTemplate.Data/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+namespace MVCTemplate.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    public sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            this.AutomaticMigrationsEnabled = false;
+            this.AutomaticMigrationDataLossAllowed = false;
+        }
+
+        protected override void Seed(MVCTemplate.Data.ApplicationDbContext context)
+        {
+            
+        }
+    }
+}
