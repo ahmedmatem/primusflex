8a924593306ab7a14424a412f6e7a2550e0eecf9
ahmed <ahmedmatem@gmail.com>
2016-05-02 11:49:21 +0100

    Extracted ApplicationDBContext and ApplicationUser

---
+++ MVCTemplate/Data/MVCTemplate.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace MVCTemplate.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using Models;
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
